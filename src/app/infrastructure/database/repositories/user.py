2c19667259eb210ef6ea5d4f67a7b362f703ce6f
yetian29 <you@example.comr>
2025-02-05 19:49:06 +0700

    Initital

--- src/app/infrastructure/database/repositories/user.py
+++ src/app/infrastructure/database/repositories/user.py
@@ -12,41 +12,31 @@ class UserRepository(IUserRepository):
         self._async_session = async_session
 
     async def get_user_by_username(self, username: str) -> UserORM | None:
-        async with self._async_session as session:
             stmt = select(UserORM).where(UserORM.username == username)
-            return await session.scalar(stmt)
+            return await self._async_session.scalar(stmt)
 
     async def get_user_by_email(self, email: str) -> UserORM | None:
-        async with self._async_session as session:
             stmt = select(UserORM).where(UserORM.email == email)
-            return await session.scalar(stmt)
+            return await self._async_session.scalar(stmt)
 
     async def get_user_by_oid(self, oid: UUID) -> UserORM | None:
-        async with self._async_session as session:
             stmt = select(UserORM).where(UserORM.oid == oid)
-            return await session.scalar(stmt)
+            return await self._async_session.scalar(stmt)
 
     async def create_user(self, user_orm: UserORM) -> UserORM:
-        async with self._async_session as session:
-            session.add(user_orm)
-            await session.commit()
-            await session.refresh(user_orm)
+            self._async_session.add(user_orm)
+            await self._async_session.refresh(user_orm)
             return user_orm
 
     async def update_user(self, user_orm: UserORM) -> UserORM:
-        async with self._async_session as session:
-            session.add(user_orm)
-            await session.commit()
-            await session.refresh(user_orm)
+            self._async_session.add(user_orm)
+            await self._async_session.refresh(user_orm)
             return user_orm
 
     async def delete_user(self, oid: UUID) -> UserORM | None:
-        async with self._async_session as session:
             stmt = select(UserORM).where(UserORM.oid == oid)
-            user = await session.scalar(stmt)
-            user_tmp = user
+            user = await self._async_session.scalar(stmt)
             if not user:
                 return None
-            await session.delete(user)
-            await session.commit()
-            return user_tmp
+            await self._async_session.delete(user)
+            return user
